3ff6c1a07fe68eb243035289255293f5914f35b6
Nikita Kolmogorov <backmeupplz@gmail.com>
2023-02-23 09:45:18 -0800

    fix typo

--- src/helpers/fetchNotifications.ts
+++ src/helpers/fetchNotifications.ts
@@ -7,8 +7,8 @@ export default function (cursor: string, bearerToken: string) {
     next?: { cursor?: string }
   }>(
     cursor
-      ? `https://api.warpcaster.com/v2/mention-and-reply-notifications?limit=10&cursor=${cursor}`
-      : 'https://api.warpcaster.com/v2/mention-and-reply-notifications?limit=10',
+      ? `https://api.warpcast.com/v2/mention-and-reply-notifications?limit=10&cursor=${cursor}`
+      : 'https://api.warpcast.com/v2/mention-and-reply-notifications?limit=10',
     {
       headers: {
         accept: 'application/json',
