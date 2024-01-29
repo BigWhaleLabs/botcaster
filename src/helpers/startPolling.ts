88cec22d963f8429e67567c12450ea8349550b45
Nikita Kolmogorov <backmeupplz@gmail.com>
2024-01-28 20:18:55 -0800

    Update startPolling.ts to use CastWithInteractions instead of Notification

--- src/helpers/startPolling.ts
+++ src/helpers/startPolling.ts
@@ -1,15 +1,15 @@
-import { Notification } from '../models/Notification'
 import {
   getCurrentNotificationId,
   setCurrentNotificationId,
 } from './currentNotificationId'
 import fetchNotifications from './fetchNotifications'
+import { CastWithInteractions } from '../../node_modules/@standard-crypto/farcaster-js-neynar/dist/commonjs/v1/openapi/generated/models/cast-with-interactions'
 
 let polling = false
 async function pollNotifications(
   fid: number,
   apiKey: string,
-  handler: (notification: Notification) => void
+  handler: (notification: CastWithInteractions) => void
 ) {
   if (polling) return
   polling = true
@@ -17,14 +17,14 @@ async function pollNotifications(
     const currentNotificationId = await getCurrentNotificationId()
     let currentNotificationIdInSet = true
     let cursor = ''
-    const notifications = [] as Notification[]
+    const notifications = [] as CastWithInteractions[]
     do {
       const {
         data: {
-          result: { notifications, next },
+          result: { notifications: newNotifications, next },
         },
       } = await fetchNotifications(fid, cursor, apiKey)
-      notifications.push(...notifications)
+      notifications.push(...newNotifications)
       if (currentNotificationId) {
         cursor = next?.cursor || ''
         currentNotificationIdInSet = notifications.some(
@@ -32,8 +32,8 @@ async function pollNotifications(
         )
       }
     } while (!!currentNotificationId && !currentNotificationIdInSet && !!cursor)
-    if (notifications[0]?.id) {
-      await setCurrentNotificationId(notifications[0].id)
+    if (notifications[0]?.hash) {
+      await setCurrentNotificationId(notifications[0].hash)
     }
     for (const notification of notifications) {
       await handler(notification)
@@ -48,7 +48,7 @@ async function pollNotifications(
 export function startPolling(
   fid: number,
   apiKey: string,
-  handler: (notification: Notification) => void
+  handler: (notification: CastWithInteractions) => void
 ) {
   void pollNotifications(fid, apiKey, handler)
   setInterval(() => pollNotifications(fid, apiKey, handler), 10 * 1000)
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,1 @@
 export * from './helpers/startPolling'
-export * from './models/Notification'
